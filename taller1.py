150289d2deae39d1a233fb5205fa3a4cefb5a69a
Juan Martinez <ju4nf3martinez@gmail.com>
2023-08-19 10:04:53 -0700

    :wrench:edad

---
+++ taller1.py
@@ -0,0 +1,11 @@
+"https://onlinegdb.com/VXC3C7shr"
+
+5
+
+
+
+edad= int(input("Cual es tu edad: "))
+if  edad>13:
+    print ("ya tienes edad para ver peliculas de ciertos temas explicitos")
+else:
+    print ("aun no puedes ver peliculas de ciertos temas explicitos")
